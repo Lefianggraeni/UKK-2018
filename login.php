e38a8645f760fdd14636e1c01aaa1c14af3c5552
lefianggraeni <levianggraeni27@gmail.com>
2018-01-29 14:05:48 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> LOGIN </title>
+</head>
+<body>
+		<form action="login.php" method="post">
+			<label>Username : </label>
+			<Input type="text" name="username" id="username"></Input>
+			<p>
+			<label> Password : </label>
+			<input type="password" name="password" id="password"></input>
+			<p>
+			<input type="submit" name="login" class="tombol">
+		</form>
+</body>
+</html>
